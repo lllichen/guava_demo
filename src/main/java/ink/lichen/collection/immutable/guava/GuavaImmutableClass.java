61c4849872a444e4fb0851fb2dc01b096ac8c6de
lichen <lichen@daojia-inc.com>
2018-11-07 12:29:00 +0800

    add new method

--- src/main/java/ink/lichen/collection/immutable/guava/GuavaImmutableClass.java
+++ src/main/java/ink/lichen/collection/immutable/guava/GuavaImmutableClass.java
@@ -1,11 +1,21 @@
 package ink.lichen.collection.immutable.guava;
 
+import com.google.common.collect.ImmutableList;
+import ink.lichen.collection.support.DemoFactory;
+
+import java.util.List;
+
 /**
  * Created by lichen@daojia.com on 2018-11-7.
  */
 public class GuavaImmutableClass {
 
     public static void main(String[] args) {
-
+        List<String> list = DemoFactory.createList();
+        ImmutableList immutableList = ImmutableList.copyOf(list);
+        System.out.println(immutableList.size());
+        list.add("a");
+        System.out.println(immutableList.size());
+        immutableList.add("a");
     }
 }
