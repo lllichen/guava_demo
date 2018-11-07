783c6c18432fbb1717f2985d8ff727a92d4ef8a6
lichen <lichen@daojia-inc.com>
2018-11-07 12:03:54 +0800

    add more

---
+++ src/main/java/ink/lichen/collection/immutable/guava/GuavaImmutableClass.java
@@ -0,0 +1,11 @@
+package ink.lichen.collection.immutable.guava;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-7.
+ */
+public class GuavaImmutableClass {
+
+    public static void main(String[] args) {
+
+    }
+}
