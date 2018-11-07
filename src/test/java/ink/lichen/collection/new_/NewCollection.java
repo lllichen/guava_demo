5e4beb5bdbd9a9a971bce56d4ced22f37efc5b4b
lichen <lichen@daojia-inc.com>
2018-11-07 15:54:27 +0800

    add rangeMap

--- src/test/java/ink/lichen/collection/new_/NewCollection.java
+++ src/test/java/ink/lichen/collection/new_/NewCollection.java
@@ -115,6 +115,16 @@ public class NewCollection {
         rangeSet.add(Range.openClosed(0, 0)); //空区间; {[1,10], [11,20)}
         rangeSet.remove(Range.open(5, 10)); //分割[1, 10]; {[1,5], [10,10], [11,20)}
 
+        rangeSet.contains(9);
+        rangeSet.rangeContaining(9);
     }
 
+    @Test
+    public void testRangeMap(){
+        RangeMap<Integer, String> rangeMap = TreeRangeMap.create();
+        rangeMap.put(Range.closed(1, 10), "foo"); //{[1,10] => "foo"}
+        rangeMap.put(Range.open(3, 6), "bar"); //{[1,3] => "foo", (3,6) => "bar", [6,10] => "foo"}
+        rangeMap.put(Range.open(10, 20), "foo"); //{[1,3] => "foo", (3,6) => "bar", [6,10] => "foo", (10,20) => "foo"}
+        rangeMap.remove(Range.closed(5, 11)); //{[1,3] => "foo", (3,5) => "bar", (11,20) => "foo"}
+    }
 }
