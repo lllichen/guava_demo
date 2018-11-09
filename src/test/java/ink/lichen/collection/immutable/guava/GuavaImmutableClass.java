a85cdccbcacd9452c0ec85321c3b92ffc8118578
lichen <lichen@daojia-inc.com>
2018-11-09 15:37:16 +0800

    add smart

--- src/test/java/ink/lichen/collection/immutable/guava/GuavaImmutableClass.java
+++ src/test/java/ink/lichen/collection/immutable/guava/GuavaImmutableClass.java
@@ -4,7 +4,9 @@ import ink.lichen.collection.support.Support;
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by lichen@daojia.com on 2018-11-7.
@@ -19,7 +21,14 @@ public class GuavaImmutableClass {
         list.add("a");
         System.out.println(immutableList.size());
         immutableList.add("a");
+
     }
 
+    @Test
+    public void testSmart(){
+        Set<String> set = new HashSet<>();
+        set.add("a");
+        ImmutableList list = ImmutableList.copyOf(set);
+    }
 
 }
